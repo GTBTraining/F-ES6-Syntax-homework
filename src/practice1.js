2bd7e0aef4d280592b7f6dff3f149523c58a30d2
“pyqpyqpyq” <alwayswannaflyx@gmail.com>
2020-11-11 14:11:53 +0800

    [pyq] implement the task2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,14 @@
-const parseData
+const parseData=(input)=>{
+    const{data,column}=input
+    const keys = column.map((c) => c.name);
+    return data.map((values)=>obj(keys,values))
+}
+
+const obj=(keys,values)=>{
+    const result = {};
+  keys.forEach((key, i) => {
+    result[key] = values[i];
+  });
+    return result;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,12 @@
-const inject
+const inject=(item,section)=>{
+    const result=[]
+    for(let i=0;i<item.length;i++){
+        const content=section.find(({index})=>index===i)
+    if (content) {
+        result.push(content.content)
+    }
+    result.push(item[i])
+    }
+    return result
+}
 export { inject };
